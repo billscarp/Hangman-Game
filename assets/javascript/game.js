fad9940fce42cdfebc813e294b4eb7d1784514e2
billscarp <billscarp@gmail.com>
2017-09-14 18:40:34 -0400

    added variables test js

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// Create Variables
+
+var wordOptions = ["porsche", "bmw", "mercedes", "bugatti", "lamborghini", "corvette", "charger", "mustang" ];
+var selectedWord = "";
+var lettersInWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongLetters = [];
+
+// counters
+
+var winCount = 0;
+var LossCount = 0;
+var guessesLeft = 9;
+
+alert("test");
